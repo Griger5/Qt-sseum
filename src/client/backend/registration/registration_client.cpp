2bc7bd0da4b6bded77f8d6ba1907471ccd3ed03e
Griger5 <griger04@gmail.com>
2026-01-23 23:41:12 +0100

    add: Registration gRPC client

---
+++ src/client/backend/registration/registration_client.cpp
@@ -0,0 +1,27 @@
+#include "client/backend/registration/registration_client.hpp"
+
+RegistrationClient::RegistrationClient(std::shared_ptr<grpc::Channel> channel) : stub_(registration::Registration::NewStub(channel)) {}
+
+RegistrationClient::RegistrationResult RegistrationClient::registerUser(const std::string &username, const std::string &email, const std::string &password) {
+    registration::RegisterRequest req;
+    registration::RegisterResponse res;
+    grpc::ClientContext ctx;
+
+    req.set_username(username);
+    req.set_email(email);
+    req.set_password(password);
+
+    grpc::Status status = this->stub_->registerUser(&ctx, req, &res);
+    
+    switch (status.error_code()) {
+        case grpc::StatusCode::OK:
+            return SUCCESS;
+            break;
+        case grpc::StatusCode::ALREADY_EXISTS:
+            return ALREADY_EXISTS;
+            break;
+        default:
+            return INTERNAL_ERROR;
+            break;
+    }
+}
---
+++ src/client/backend/registration/registration_client.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <string>
+#include <memory>
+
+#include <grpcpp/grpcpp.h>
+
+#include "proto/registration.grpc.pb.h"
+
+class RegistrationClient {
+public:
+    enum RegistrationResult {
+        SUCCESS,
+        ALREADY_EXISTS,
+        INTERNAL_ERROR
+    };
+
+    explicit RegistrationClient(std::shared_ptr<grpc::Channel> channel);
+    RegistrationResult registerUser(const std::string &username, const std::string &email, const std::string &password);
+
+private:
+    std::unique_ptr<registration::Registration::Stub> stub_;
+};
