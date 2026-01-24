e9c2b79d4b6da35da35594703a6df4419660cb07
Griger5 <griger04@gmail.com>
2026-01-24 23:35:38 +0100

    add: role column in users table

--- init_db.sql
+++ init_db.sql
@@ -1,6 +1,9 @@
+CREATE TYPE user_role AS ENUM ('user', 'admin');
+
 CREATE TABLE users (
     id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     username text NOT NULL,
     email text NOT NULL,
-    password_hash text NOT NULL
+    password_hash text NOT NULL,
+    role user_role NOT NULL DEFAULT 'user'
 );
