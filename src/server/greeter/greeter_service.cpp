e24bf316f178494c493b205e33996297d776c420
Griger5 <griger04@gmail.com>
2026-01-26 16:35:28 +0100

    update: verify auth in GreeterService

--- src/server/greeter/greeter_service.cpp
+++ src/server/greeter/greeter_service.cpp
@@ -1,6 +1,12 @@
 #include "server/greeter/greeter_service.hpp"
 
+#include "utils/verify_auth.hpp"
+
 grpc::Status GreeterService::sayHello(grpc::ServerContext *context, const greeter::HelloRequest *request, greeter::HelloReply *reply) {
+    if (!utils::verifyAuth(context)) {
+        return grpc::Status(grpc::StatusCode::UNAUTHENTICATED, "Invalid token");
+    }
+
     reply->set_message("Hello " + request->name());
 
     return grpc::Status::OK;
